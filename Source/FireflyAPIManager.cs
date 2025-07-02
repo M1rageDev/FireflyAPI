938bd08ceed4fed9b6f4339c5539caec388386a4
MirageDev <forpixel.studio@gmail.com>
2025-07-02 18:21:59 +0200

    Add project files.

---
+++ Source/FireflyAPIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FireflyAPI
+{
+	public static class FireflyAPIManager
+	{
+		public static bool IsFireflyInstalled { get; set; }
+		public static Assembly FireflyAssembly { get; set; }
+
+		static FireflyAPIManager()
+		{
+			IsFireflyInstalled = AssemblyLoader.loadedAssemblies.Contains("Firefly");
+			if (!IsFireflyInstalled) return;
+
+			FireflyAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.name.Equals("Firefly", StringComparison.OrdinalIgnoreCase)).assembly;
+		}
+
+		public static bool TryFindModule(Vessel vessel, out IFireflyModule module)
+		{
+			if (vessel == null)
+			{
+				module = null;
+				return false;
+			}
+
+			module = vessel.FindVesselModuleImplementing<IFireflyModule>();
+			return module == null;
+		}
+	}
+}
---
+++ Source/IFireflyModule.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FireflyAPI
+{
+	public interface IFireflyModule
+	{
+		bool OverridePhysics { get; set; }
+
+		string OverridenBy { get; set; }
+		Vector3 OverrideEntryDirection { get; set; }
+		float OverrideEffectStrength { get; set; }
+		float OverrideEffectState { get; set; }
+		float OverrideAngleOfAttack { get; set; }
+		string OverrideBodyConfigName { get; set; }
+
+		void ResetOverride();
+		void ReloadCommandBuffer();
+		void CreateVesselFx();
+	}
+}
